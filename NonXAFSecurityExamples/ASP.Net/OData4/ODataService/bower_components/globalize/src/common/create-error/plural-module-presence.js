eda8706580272dbbcdad26a6fa7d24b1f8717e48
Anton Shabunin <anton.shabunin@devexpress.com>
2019-06-14 09:15:20 +0300

    Remove bower components

--- NonXAFSecurityExamples/ASP.Net/OData4/ODataService/bower_components/globalize/src/common/create-error/plural-module-presence.js
+++
@@ -1,9 +0,0 @@
-define([
-	"../create-error"
-], function( createError ) {
-
-return function() {
-	return createError( "E_MISSING_PLURAL_MODULE", "Plural module not loaded." );
-};
-
-});
--- NonXAFSecurityExamples/ASP.Net/OData4/ODataService/bower_components/globalize/src/date/expand-pattern/normalize-pattern-type.js
+++
@@ -1,9 +0,0 @@
-define([
-	"./similar-fields-map"
-], function( dateExpandPatternSimilarFieldsMap ) {
-
-return function( character ) {
-	return dateExpandPatternSimilarFieldsMap[ character ] || character;
-};
-
-});
