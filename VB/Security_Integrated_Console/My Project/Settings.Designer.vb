68c448615334a0ec3a0ae01046884a55523cd424
DevExpressExampleBot <fakeEmail>
2019-06-14 08:44:15 +0300

    Source auto update [skip ci]

--- VB/Security_Integrated_Console/My Project/Settings.Designer.vb
+++ VB/Security_Integrated_Console/My Project/Settings.Designer.vb
@@ -11,16 +11,17 @@
 Namespace My
 
 
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "9.0.0.0")> _
-    Friend NotInheritable Partial Class Settings
-        Inherits System.Configuration.ApplicationSettingsBase
+	<Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>
+	<Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "9.0.0.0")>
+	Friend NotInheritable Partial Class Settings
+		Inherits System.Configuration.ApplicationSettingsBase
 
-        Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
+		Private Shared defaultInstance As Settings = (CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()), Settings))
 
-        Public Shared ReadOnly Property [Default]() As Settings
-            Get
-                Return defaultInstance
-            End Get
-        End Property
-    End Class
+		Public Shared ReadOnly Property [Default]() As Settings
+			Get
+				Return defaultInstance
+			End Get
+		End Property
+	End Class
 End Namespace
--- VB/Security_Integrated_Console/Program.vb
+++ VB/Security_Integrated_Console/Program.vb
@@ -11,77 +11,75 @@ Imports DevExpress.Persistent.BaseImpl.PermissionPolicy
 Imports DevExpress.Persistent.Base
 
 Namespace Security_Integrated_Console.Win
-    Friend NotInheritable Class Program
+	Friend Module Program
+		Private Sub UpdateDatabase(ByVal objectSpace As IObjectSpace)
+			Dim userAdmin As PermissionPolicyUser = objectSpace.CreateObject(Of PermissionPolicyUser)()
+			userAdmin.UserName = "Admin"
+			userAdmin.SetPassword("")
+			Dim adminRole As PermissionPolicyRole = objectSpace.CreateObject(Of PermissionPolicyRole)()
+			adminRole.IsAdministrative = True
+			userAdmin.Roles.Add(adminRole)
 
-        Private Sub New()
-        End Sub
+			Dim userJohn As PermissionPolicyUser = objectSpace.CreateObject(Of PermissionPolicyUser)()
+			userJohn.UserName = "User"
+			Dim userRole As PermissionPolicyRole = objectSpace.FindObject(Of PermissionPolicyRole)(New BinaryOperator("Name", "Users"))
+			userRole = objectSpace.CreateObject(Of PermissionPolicyRole)()
+			userRole.AddObjectPermission(Of Person)(SecurityOperations.Read, "[FirstName] == 'User person'", SecurityPermissionState.Allow)
+			userJohn.Roles.Add(userRole)
 
-        Private Shared Sub UpdateDatabase(ByVal objectSpace As IObjectSpace)
-            Dim userAdmin As PermissionPolicyUser = objectSpace.CreateObject(Of PermissionPolicyUser)()
-            userAdmin.UserName = "Admin"
-            userAdmin.SetPassword("")
-            Dim adminRole As PermissionPolicyRole = objectSpace.CreateObject(Of PermissionPolicyRole)()
-            adminRole.IsAdministrative = True
-            userAdmin.Roles.Add(adminRole)
+			Dim adminPerson As Person = objectSpace.FindObject(Of Person)(New BinaryOperator("FirstName", "Person for Admin"))
+			adminPerson = objectSpace.CreateObject(Of Person)()
+			adminPerson.FirstName = "Admin person"
 
-            Dim userJohn As PermissionPolicyUser = objectSpace.CreateObject(Of PermissionPolicyUser)()
-            userJohn.UserName = "User"
-            Dim userRole As PermissionPolicyRole = objectSpace.FindObject(Of PermissionPolicyRole)(New BinaryOperator("Name", "Users"))
-            userRole = objectSpace.CreateObject(Of PermissionPolicyRole)()
-            userRole.AddObjectPermission(Of Person)(SecurityOperations.Read, "[FirstName] == 'User person'", SecurityPermissionState.Allow)
-            userJohn.Roles.Add(userRole)
+			Dim userPerson As Person = objectSpace.CreateObject(Of Person)()
+			userPerson.FirstName = "User person"
+			objectSpace.CommitChanges()
+		End Sub
 
-            Dim adminPerson As Person = objectSpace.FindObject(Of Person)(New BinaryOperator("FirstName", "Person for Admin"))
-            adminPerson = objectSpace.CreateObject(Of Person)()
-            adminPerson.FirstName = "Admin person"
+		<STAThread>
+		Sub Main()
+			XpoTypesInfoHelper.GetXpoTypeInfoSource()
+			XafTypesInfo.Instance.RegisterEntity(GetType(Person))
+			XafTypesInfo.Instance.RegisterEntity(GetType(PermissionPolicyUser))
+			XafTypesInfo.Instance.RegisterEntity(GetType(PermissionPolicyRole))
 
-            Dim userPerson As Person = objectSpace.CreateObject(Of Person)()
-            userPerson.FirstName = "User person"
-            objectSpace.CommitChanges()
-        End Sub
+			Console.WriteLine("Update database...")
+			Dim dataSet As New DataSet()
+			Console.WriteLine("Database has been updated successfully.")
 
-        <STAThread> _
-        Shared Sub Main()
-            XpoTypesInfoHelper.GetXpoTypeInfoSource()
-            XafTypesInfo.Instance.RegisterEntity(GetType(Person))
-            XafTypesInfo.Instance.RegisterEntity(GetType(PermissionPolicyUser))
-            XafTypesInfo.Instance.RegisterEntity(GetType(PermissionPolicyRole))
+			Dim directProvider As New XPObjectSpaceProvider(New MemoryDataStoreProvider(dataSet))
+			Dim directObjectSpace As IObjectSpace = directProvider.CreateObjectSpace()
+			UpdateDatabase(directObjectSpace)
 
-            Console.WriteLine("Update database...")
-            Dim dataSet As New DataSet()
-            Console.WriteLine("Database has been updated successfully.")
+			Dim auth As New AuthenticationStandard()
+			Dim security As New SecurityStrategyComplex(GetType(PermissionPolicyUser), GetType(PermissionPolicyRole), auth)
+			SecuritySystem.SetInstance(security)
+			Dim osProvider As New SecuredObjectSpaceProvider(security, New MemoryDataStoreProvider(dataSet))
 
-            Dim directProvider As New XPObjectSpaceProvider(New MemoryDataStoreProvider(dataSet))
-            Dim directObjectSpace As IObjectSpace = directProvider.CreateObjectSpace()
-            UpdateDatabase(directObjectSpace)
+			auth.SetLogonParameters(New AuthenticationStandardLogonParameters("User", ""))
+			Console.WriteLine("Logging 'User' user...")
+			security.Logon(directObjectSpace)
+			Console.WriteLine("'User' is logged on.")
+			Console.WriteLine("List of the 'Person' objects:")
+			Using securedObjectSpace As IObjectSpace = osProvider.CreateObjectSpace()
+				For Each person As Person In securedObjectSpace.GetObjects(Of Person)()
+					Console.WriteLine(person.FirstName)
+				Next person
+			End Using
 
-            Dim auth As New AuthenticationStandard()
-            Dim security As New SecurityStrategyComplex(GetType(PermissionPolicyUser), GetType(PermissionPolicyRole), auth)
-            SecuritySystem.SetInstance(security)
-            Dim osProvider As New SecuredObjectSpaceProvider(security, New MemoryDataStoreProvider(dataSet))
-            Dim securedObjectSpace As IObjectSpace = osProvider.CreateObjectSpace()
+			auth.SetLogonParameters(New AuthenticationStandardLogonParameters("Admin", ""))
+			Console.WriteLine("Logging 'Admin' user...")
+			security.Logon(directObjectSpace)
+			Console.WriteLine("Admin is logged on.")
+			Console.WriteLine("List of the 'Person' objects:")
+			Using securedObjectSpace As IObjectSpace = osProvider.CreateObjectSpace()
+				For Each person As Person In securedObjectSpace.GetObjects(Of Person)()
+					Console.WriteLine(person.FirstName)
+				Next person
+			End Using
 
-            auth.SetLogonParameters(New AuthenticationStandardLogonParameters("User", ""))
-            Console.WriteLine("Logging 'User' user...")
-            security.Logon(directObjectSpace)
-            Console.WriteLine("'User' is logged on.")
-            Console.WriteLine("List of the 'Person' objects:")
-            For Each person As Person In securedObjectSpace.GetObjects(Of Person)()
-                Console.WriteLine(person.FirstName)
-            Next person
-
-            auth.SetLogonParameters(New AuthenticationStandardLogonParameters("Admin", ""))
-            Console.WriteLine("Logging 'Admin' user...")
-            security.Logon(directObjectSpace)
-            Console.WriteLine("Admin is logged on.")
-            securedObjectSpace = osProvider.CreateObjectSpace()
-            Console.WriteLine("List of the 'Person' objects:")
-            For Each person As Person In securedObjectSpace.GetObjects(Of Person)()
-                Console.WriteLine(person.FirstName)
-            Next person
-
-            Console.WriteLine("Press enter to exit...")
-            Console.ReadLine()
-        End Sub
-    End Class
+			Console.WriteLine("Press enter to exit...")
+			Console.ReadLine()
+		End Sub
+	End Module
 End Namespace
