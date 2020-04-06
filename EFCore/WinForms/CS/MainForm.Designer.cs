8f5d13e849f8701890f447f03e08949d4964c050
TikhomirovP <pavel.tikhomirov@devexpress.com>
2020-04-06 19:42:11 +0300

    Add winforms app

---
+++ EFCore/WinForms/CS/MainForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApplication {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+			this.logoutButtonItem = new DevExpress.XtraBars.BarButtonItem();
+			this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+			this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+			this.documentManager1 = new DevExpress.XtraBars.Docking2010.DocumentManager(this.components);
+			this.nativeMdiView1 = new DevExpress.XtraBars.Docking2010.Views.NativeMdi.NativeMdiView(this.components);
+			this.barAndDockingController1 = new DevExpress.XtraBars.BarAndDockingController(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.documentManager1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nativeMdiView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.barAndDockingController1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// ribbonControl1
+			// 
+			this.ribbonControl1.AllowMdiChildButtons = false;
+			this.ribbonControl1.CommandLayout = DevExpress.XtraBars.Ribbon.CommandLayout.Simplified;
+			this.ribbonControl1.Controller = this.barAndDockingController1;
+			this.ribbonControl1.ExpandCollapseItem.Id = 0;
+			this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+			this.ribbonControl1.ExpandCollapseItem,
+			this.ribbonControl1.SearchEditItem,
+			this.logoutButtonItem});
+			this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+			this.ribbonControl1.MaxItemId = 2;
+			this.ribbonControl1.MdiMergeStyle = DevExpress.XtraBars.Ribbon.RibbonMdiMergeStyle.Always;
+			this.ribbonControl1.Name = "ribbonControl1";
+			this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+			this.ribbonPage1});
+			this.ribbonControl1.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+			this.ribbonControl1.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
+			this.ribbonControl1.Size = new System.Drawing.Size(685, 82);
+			this.ribbonControl1.ToolbarLocation = DevExpress.XtraBars.Ribbon.RibbonQuickAccessToolbarLocation.Hidden;
+			// 
+			// logoffButtonItem
+			// 
+			this.logoutButtonItem.Caption = "Log Out";
+			this.logoutButtonItem.Id = 1;
+			this.logoutButtonItem.ImageOptions.SvgImage = global::WindowsFormsApplication.Properties.Resources.Action_Logoff;
+			this.logoutButtonItem.Name = "logoffButtonItem";
+			this.logoutButtonItem.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.LogoutButtonItem_ItemClick);
+			// 
+			// ribbonPage1
+			// 
+			this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+			this.ribbonPageGroup1});
+			this.ribbonPage1.Name = "ribbonPage1";
+			this.ribbonPage1.Text = "Home";
+			// 
+			// ribbonPageGroup1
+			// 
+			this.ribbonPageGroup1.AllowTextClipping = false;
+			this.ribbonPageGroup1.ItemLinks.Add(this.logoutButtonItem);
+			this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+			this.ribbonPageGroup1.Text = "General";
+			// 
+			// documentManager1
+			// 
+			this.documentManager1.BarAndDockingController = this.barAndDockingController1;
+			this.documentManager1.MdiParent = this;
+			this.documentManager1.MenuManager = this.ribbonControl1;
+			this.documentManager1.View = this.nativeMdiView1;
+			this.documentManager1.ViewCollection.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseView[] {
+			this.nativeMdiView1});
+			// 
+			// barAndDockingController1
+			// 
+			this.barAndDockingController1.PropertiesRibbon.DefaultSimplifiedRibbonGlyphSize = 32;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(685, 600);
+			this.Controls.Add(this.ribbonControl1);
+			this.Icon = global::WindowsFormsApplication.Properties.Resources.ExpressApp;
+			this.IsMdiContainer = true;
+			this.Name = "MainForm";
+			this.Ribbon = this.ribbonControl1;
+			this.Text = "XAF\'s Security in Non-XAF Apps Demo";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.documentManager1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nativeMdiView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.barAndDockingController1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+		private DevExpress.XtraBars.BarButtonItem logoutButtonItem;
+		private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+		private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+		private DevExpress.XtraBars.Docking2010.DocumentManager documentManager1;
+		private DevExpress.XtraBars.Docking2010.Views.NativeMdi.NativeMdiView nativeMdiView1;
+		private DevExpress.XtraBars.BarAndDockingController barAndDockingController1;
+	}
+}
---
+++ EFCore/WinForms/VB/EmployeeDetailForm.vb
@@ -0,0 +1,95 @@
+﻿Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.DC
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Win.Editors
+Imports DevExpress.Xpo
+Imports DevExpress.XtraEditors
+Imports DevExpress.XtraLayout
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+Imports XafSolution.Module.BusinessObjects
+
+Namespace WindowsFormsApplication
+    Partial Public Class EmployeeDetailForm
+        Inherits DevExpress.XtraBars.Ribbon.RibbonForm
+
+        Private securedObjectSpace As IObjectSpace
+        Private security As SecurityStrategyComplex
+        Private objectSpaceProvider As IObjectSpaceProvider
+        Private employee As Employee
+        Private visibleMembers As Dictionary(Of String, String)
+        Public Sub New(ByVal employee As Employee)
+            InitializeComponent()
+            Me.employee = employee
+            visibleMembers = New Dictionary(Of String, String)()
+            visibleMembers.Add(nameof(XafSolution.Module.BusinessObjects.Employee.FirstName), "First Name:")
+            visibleMembers.Add(nameof(XafSolution.Module.BusinessObjects.Employee.LastName), "Last Name:")
+            visibleMembers.Add(nameof(XafSolution.Module.BusinessObjects.Employee.Department), "Department:")
+        End Sub
+        Private Sub EmployeeDetailForm_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            security = CType(MdiParent, MainForm).Security
+            objectSpaceProvider = CType(MdiParent, MainForm).ObjectSpaceProvider
+            securedObjectSpace = objectSpaceProvider.CreateObjectSpace()
+            If employee Is Nothing Then
+                employee = securedObjectSpace.CreateObject(Of Employee)()
+            Else
+                employee = securedObjectSpace.GetObject(employee)
+                deleteBarButtonItem.Enabled = security.CanDelete(employee)
+            End If
+            employeeBindingSource.DataSource = employee
+            AddControls()
+        End Sub
+        Private Sub AddControls()
+            For Each pair As KeyValuePair(Of String, String) In visibleMembers
+                Dim memberName As String = pair.Key
+                Dim caption As String = pair.Value
+                AddControl(dataLayoutControl1.AddItem(), employee, memberName, caption)
+            Next pair
+        End Sub
+        Private Sub AddControl(ByVal layout As LayoutControlItem, ByVal targetObject As Object, ByVal memberName As String, ByVal caption As String)
+            layout.Text = caption
+            Dim type As Type = targetObject.GetType()
+            Dim control As BaseEdit
+            If security.CanRead(targetObject, memberName) Then
+                control = GetControl(type, memberName)
+                If control IsNot Nothing Then
+                    control.DataBindings.Add(New Binding("EditValue", employeeBindingSource, memberName, True, DataSourceUpdateMode.OnPropertyChanged))
+                    control.Enabled = security.CanWrite(targetObject, memberName)
+                End If
+            Else
+                control = New ProtectedContentEdit()
+                control.Enabled = False
+            End If
+            dataLayoutControl1.Controls.Add(control)
+            layout.Control = control
+        End Sub
+        Private Function GetControl(ByVal type As Type, ByVal memberName As String) As BaseEdit
+            Dim control As BaseEdit = Nothing
+            Dim typeInfo As ITypeInfo = securedObjectSpace.TypesInfo.FindTypeInfo(type)
+            Dim memberInfo As IMemberInfo = typeInfo.FindMember(memberName)
+            If memberInfo IsNot Nothing Then
+                If memberInfo.IsAssociation Then
+                    control = New ComboBoxEdit()
+                    CType(control, ComboBoxEdit).Properties.Items.AddRange(TryCast(securedObjectSpace.GetObjects(Of Department)(), XPCollection(Of Department)))
+                Else
+                    control = New TextEdit()
+                End If
+            End If
+            Return control
+        End Function
+        Private Sub SaveBarButtonItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles saveBarButtonItem.ItemClick
+            securedObjectSpace.CommitChanges()
+            Close()
+        End Sub
+        Private Sub CloseBarButtonItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles closeBarButtonItem.ItemClick
+            Close()
+        End Sub
+        Private Sub DeleteBarButtonItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles deleteBarButtonItem.ItemClick
+            securedObjectSpace.Delete(employee)
+            securedObjectSpace.CommitChanges()
+            Close()
+        End Sub
+    End Class
+End Namespace
