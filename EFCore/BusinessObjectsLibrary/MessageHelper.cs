5625f22ac35699eea695f64ab294456b03673e85
CORP\garavsky <dennis@devexpress.com>
2020-05-03 23:05:29 +0300

    Remove unnecessary files

--- EFCore/BusinessObjectsLibrary/MessageHelper.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace BusinessObjectsLibrary.EFCore.NetCore {
-    public static class MessageHelper {
-        public const string OpenDatabaseFailed = "Make sure the database has been created.\r\nTo create the database, run the 'DatabaseUpdater' project.";
-    }
-}
