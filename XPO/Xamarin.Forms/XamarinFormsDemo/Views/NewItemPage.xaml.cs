dd7e6a3dc57e98d0663fe57f697538af02609f01
trenin.georgiy <georgiy.trenin@devexpress.com>
2020-11-26 16:40:15 +0300

    Xamarin.Forms

---
+++ XPO/Xamarin.Forms/XamarinFormsDemo/Views/NewItemPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DevExpress.Xpo;
+using XafSolution.Module.BusinessObjects;
+using Xamarin.Forms;
+
+namespace XamarinFormsDemo.Views {
+    public partial class NewItemPage : ContentPage {
+        public Employee Item { get; set; }
+
+        public NewItemPage() {
+            InitializeComponent();
+
+            Item = new Employee(XpoDefault.Session) {
+                FirstName = "First name",
+                LastName = "Last name"
+            };
+
+            BindingContext = this;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e) {
+            MessagingCenter.Send(this, "AddItem", Item);
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
