3ab1632e56fb98e4b1bc121f79c1d7a93544daac
trenin.georgiy <georgiy.trenin@devexpress.com>
2020-11-30 18:54:22 +0300

    Research UI

--- XPO/Xamarin.Forms/XamarinFormsDemo/ViewModels/ItemDetailViewModel.cs
+++ XPO/Xamarin.Forms/XamarinFormsDemo/ViewModels/ItemDetailViewModel.cs
@@ -1,10 +1,25 @@
-﻿using DevExpress.ExpressApp.Security;
+﻿using DevExpress.Data.Extensions;
+using DevExpress.ExpressApp.Security;
+using DevExpress.Xpo;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using XafSolution.Module.BusinessObjects;
 using Xamarin.Forms;
 
 namespace XamarinFormsDemo.ViewModels {
     public class ItemDetailViewModel : BaseViewModel {
         public Employee Item { get; set; }
+        public int Department {
+            get { return department; }
+            set { SetProperty(ref department, value);  }
+        }
+        int department;
+        public List<Department> Departments {
+            get { return departments; }
+            set { SetProperty(ref departments, value); }
+        }
+        List<Department> departments;
+
         public ItemDetailViewModel(Employee item = null) {
             Title = item?.FullName;
             Item = item;
@@ -16,8 +31,12 @@ namespace XamarinFormsDemo.ViewModels {
         () => CheckUpdate);
             CheckDelete = XpoHelper.security.CanDelete(Item);
             CheckUpdate = XpoHelper.security.CanWrite(Item);
-            
-
+            if(Item.Department != null) {
+                Task<List<Department>> myTask = Task.Run(() => task());
+                myTask.Wait();
+                Departments = myTask.Result;
+                Department = Departments.IndexOf((Department)Item.Department);
+            }
         }
         public bool CheckDelete {
             get { return checkDelete; }
@@ -31,5 +50,9 @@ namespace XamarinFormsDemo.ViewModels {
         bool checkDelete;
         public Command TempCommandDelete { get; private set; }
         public Command TempCommandUpdate { get; private set; }
+
+        async Task<List<Department>> task() {
+            return await ((UnitOfWork)Item.Session).Query<Department>().ToListAsync();
+        }
     }
 }
