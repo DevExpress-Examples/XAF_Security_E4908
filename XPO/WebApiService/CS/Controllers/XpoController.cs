e8cb26ddcb202d5efa607441de1a602e54b976af
Uriah <uriah@devexpress.com>
2020-12-14 13:50:37 +0300

    removed unused usings and comments

--- XPO/WebApiService/CS/Controllers/XpoController.cs
+++ XPO/WebApiService/CS/Controllers/XpoController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DevExpress.Xpo.DB;
+﻿using DevExpress.Xpo.DB;
+
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+
+using System.Threading.Tasks;
 
 namespace WebApiService.Controllers {
     [ApiController]
--- XPO/WebApiService/CS/Startup.cs
+++ XPO/WebApiService/CS/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DevExpress.Xpo;
 using DevExpress.Xpo.DB;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApiService {
     public class Startup {
@@ -32,7 +26,6 @@ namespace WebApiService {
             // add XML Serializer formatters
             services.AddMvc()
                 .AddXmlSerializerFormatters();
-            // b'{}'
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
