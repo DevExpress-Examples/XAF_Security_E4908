f61940f9db69ceda1bf84934f2d474c2b2abb99c
Uriah <uriah@devexpress.com>
2020-12-14 13:53:23 +0300

    added startup page to webapi service

--- XPO/WebApiService/CS/Controllers/XpoController.cs
+++ XPO/WebApiService/CS/Controllers/XpoController.cs
@@ -12,6 +12,10 @@ namespace WebApiService.Controllers {
         public XpoController(WebApiDataStoreService dataStoreService) {
             this.DataStoreService = dataStoreService;
         }
+        [HttpGet]
+        public string Hello() {
+            return "Use WebApiDataStoreClient to connect to this service. See also: https://docs.devexpress.com/XPO/402182/connect-to-a-data-store/transfer-data-via-rest-api";
+        }
         [HttpPost]
         public Task<OperationResult<UpdateSchemaResult>> UpdateSchema([FromQuery] bool doNotCreateIfFirstTableNotExist, [FromBody] DBTable[] tables) {
             return DataStoreService.UpdateSchemaAsync(doNotCreateIfFirstTableNotExist, tables);
