1c3186f234a8de6b5a8e13cf0e0be4748caf161f
trenin.georgiy <georgiy.trenin@devexpress.com>
2021-07-20 16:53:13 +0300

    updater modified

--- XPO/DatabaseUpdater/Program.cs
+++ XPO/DatabaseUpdater/Program.cs
@@ -19,8 +19,8 @@ namespace DatabaseUpdater {
             Console.WriteLine("Starting database update...");
 
             using(IObjectSpace objectSpace = objectSpaceProvider.CreateUpdatingObjectSpace(true)) {
-                Updater updater = new Updater(objectSpace, null);
-                updater.UpdateDatabaseAfterUpdateSchema();
+                Updater updater = new Updater(objectSpace);
+                updater.UpdateDatabase();
             }
 
             Console.WriteLine("Database is updated. Press any key to close.");
