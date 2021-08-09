d15abd4d564c9abcba6f44a69c86919383724ff2
AlexanderZhyrkov <alexander.zhyrkov@devexpress.com>
2021-08-09 11:58:37 +0300

    synchronization of SecurityProvider at xpo

--- XPO/ASP.NetCore/DevExtreme.OData/Helpers/XpoDataStoreProviderService.cs
+++ XPO/ASP.NetCore/DevExtreme.OData/Helpers/XpoDataStoreProviderService.cs
@@ -1,12 +1,17 @@
 ﻿using DevExpress.ExpressApp.Xpo;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 
 namespace ASPNETCoreODataService {
 	public class XpoDataStoreProviderService {
 		private IXpoDataStoreProvider dataStoreProvider;
-		public IXpoDataStoreProvider GetDataStoreProvider(string connectionString, IDbConnection connection, bool enablePoolingInConnectionString) {
+		private string connectionString;
+		public XpoDataStoreProviderService(IConfiguration config) {
+			connectionString = config.GetConnectionString("ConnectionString");
+		}
+		public IXpoDataStoreProvider GetDataStoreProvider() {
 			if(dataStoreProvider == null) {
-				dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, enablePoolingInConnectionString);
+				dataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, null, true);
 			}
 			return dataStoreProvider;
 		}
--- XPO/ASP.NetCore/MVC/Helpers/SecurityProvider.cs
+++ XPO/ASP.NetCore/MVC/Helpers/SecurityProvider.cs
@@ -6,25 +6,30 @@ using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 
 namespace MvcApplication {
-	public class SecurityProvider: IDisposable {
+	public class SecurityProvider : IDisposable {
 		public SecurityStrategyComplex Security { get; private set; }
 		public IObjectSpaceProvider ObjectSpaceProvider { get; private set; }
-		XpoDataStoreProviderService xpoDataStoreProviderService;
-		IConfiguration config;
-		IHttpContextAccessor contextAccessor;
-		public SecurityProvider(XpoDataStoreProviderService xpoDataStoreProviderService, IConfiguration config, IHttpContextAccessor contextAccessor) {
+		private XpoDataStoreProviderService xpoDataStoreProviderService;
+		private IHttpContextAccessor contextAccessor;
+		public SecurityProvider(XpoDataStoreProviderService xpoDataStoreProviderService, IHttpContextAccessor contextAccessor) {
 			this.xpoDataStoreProviderService = xpoDataStoreProviderService;
-			this.config = config;
 			this.contextAccessor = contextAccessor;
 			if(contextAccessor.HttpContext.User.Identity.IsAuthenticated) {
 				Initialize();
 			}
+		}
+		private void SignIn(HttpContext httpContext, string userName) {
+			List<Claim> claims = new List<Claim>{
+				new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+			};
+			ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+			ClaimsPrincipal principal = new ClaimsPrincipal(id);
+			httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
 		}
 		public bool InitConnection(string userName, string password) {
 			AuthenticationStandardLogonParameters parameters = new AuthenticationStandardLogonParameters(userName, password);
@@ -44,14 +49,6 @@ namespace MvcApplication {
 			ObjectSpaceProvider = GetObjectSpaceProvider(Security);
 			Login(Security, ObjectSpaceProvider);
 		}
-		private void SignIn(HttpContext httpContext, string userName) {
-			List<Claim> claims = new List<Claim>{
-				new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
-			};
-			ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
-			ClaimsPrincipal principal = new ClaimsPrincipal(id);
-			httpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-		}
 		private SecurityStrategyComplex GetSecurity(string authenticationName, object parameter) {
 			AuthenticationMixed authentication = new AuthenticationMixed();
 			authentication.LogonParametersType = typeof(AuthenticationStandardLogonParameters);
@@ -63,8 +60,7 @@ namespace MvcApplication {
 			return security;
 		}
 		private IObjectSpaceProvider GetObjectSpaceProvider(SecurityStrategyComplex security) {
-			string connectionString = config.GetConnectionString("ConnectionString");
-			SecuredObjectSpaceProvider objectSpaceProvider = new SecuredObjectSpaceProvider(security, xpoDataStoreProviderService.GetDataStoreProvider(connectionString, null, true), true);
+			SecuredObjectSpaceProvider objectSpaceProvider = new SecuredObjectSpaceProvider(security, xpoDataStoreProviderService.GetDataStoreProvider(), true);
 			RegisterEntities(objectSpaceProvider);
 			return objectSpaceProvider;
 		}
