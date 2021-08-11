8f23f9e6545b267c7e037dfb49315aeaf7880679
AlexanderZhyrkov <alexander.zhyrkov@devexpress.com>
2021-08-11 11:57:37 +0300

    mvc efcore fix services

--- EFCore/ASP.NetCore/MVC/Startup.cs
+++ EFCore/ASP.NetCore/MVC/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessObjectsLibrary.EFCore.BusinessObjects;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,53 +20,40 @@ namespace MvcApplication {
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc(options => {
                 options.EnableEndpointRouting = false;
-            })
-
-                .SetCompatibilityVersion(CompatibilityVersion.Latest);
-
+            }).SetCompatibilityVersion(CompatibilityVersion.Latest);
             services.AddControllers().
             AddNewtonsoftJson(options => {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 options.SerializerSettings.ContractResolver = new DefaultContractResolver();
             });
-
-
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                      .AddCookie(options => {
                          options.LoginPath = loginPath;
                      });
-
-
-            services.AddScoped((serviceProvider) =>
-            serviceProvider.GetRequiredService<SecurityStrategyComplexProvider>().GetSecurity());
-            services.AddScoped<ISelectDataSecurityProvider>((serviceProvider) =>
-            serviceProvider.GetRequiredService<SecurityStrategyComplexProvider>().GetSecurity());
             services.AddDbContextFactory<ApplicationDbContext>((serviceProvider, options) => {
-                // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
-                // Do not use this code in production environment to avoid data loss.
-                // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
-                //options.UseInMemoryDatabase("InMemory");
                 string connectionString = Configuration.GetConnectionString("ConnectionString");
                 options.UseSqlServer(connectionString);
                 options.UseLazyLoadingProxies();
-                options.UseSecurity(serviceProvider);
+                options.UseSecurity(serviceProvider.GetRequiredService<SecurityStrategyComplex>(), XafTypesInfo.Instance);
             }, ServiceLifetime.Scoped);
-            services.AddSingleton(Configuration);
             services.AddHttpContextAccessor();
             services.AddScoped<SecurityProvider>();
-            services.AddScoped<SecurityStrategyComplexProvider>();
-
+            services.AddScoped((serviceProvider) => {
+                AuthenticationMixed authentication = new AuthenticationMixed();
+                authentication.LogonParametersType = typeof(AuthenticationStandardLogonParameters);
+                authentication.AddAuthenticationStandardProvider(typeof(PermissionPolicyUser));
+                authentication.AddIdentityAuthenticationProvider(typeof(PermissionPolicyUser));
+                SecurityStrategyComplex security = new SecurityStrategyComplex(typeof(PermissionPolicyUser), typeof(PermissionPolicyRole), authentication);
+                return security;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if(env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
