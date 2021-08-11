d85032c47679407004565ff018949f78ea92c104
CORP\tikhomirov.pavel <pavel.tikhomirov@devexpress.com>
2021-08-11 13:40:58 +0300

    fix MvcApplication

--- EFCore/ASP.NetCore/MVC/Startup.cs
+++ EFCore/ASP.NetCore/MVC/Startup.cs
@@ -1,5 +1,7 @@
-﻿using BusinessObjectsLibrary.EFCore.BusinessObjects;
+﻿using BusinessObjectsLibrary.BusinessObjects;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,9 +20,7 @@ namespace MvcApplication {
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc(options => {
@@ -31,29 +31,16 @@ namespace MvcApplication {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 options.SerializerSettings.ContractResolver = new DefaultContractResolver();
             });
-
-
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                      .AddCookie(options => {
                          options.LoginPath = loginPath;
                      });
-
-
-            services.AddScoped((serviceProvider) =>
-            serviceProvider.GetRequiredService<SecurityStrategyComplexProvider>().GetSecurity());
-            services.AddScoped<ISelectDataSecurityProvider>((serviceProvider) =>
-            serviceProvider.GetRequiredService<SecurityStrategyComplexProvider>().GetSecurity());
             services.AddDbContextFactory<ApplicationDbContext>((serviceProvider, options) => {
-                // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
-                // Do not use this code in production environment to avoid data loss.
-                // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
-                //options.UseInMemoryDatabase("InMemory");
                 string connectionString = Configuration.GetConnectionString("ConnectionString");
                 options.UseSqlServer(connectionString);
                 options.UseLazyLoadingProxies();
                 options.UseSecurity(serviceProvider.GetRequiredService<SecurityStrategyComplex>(), XafTypesInfo.Instance);
             }, ServiceLifetime.Scoped);
-            services.AddSingleton(Configuration);
             services.AddHttpContextAccessor();
             services.AddScoped<SecurityProvider>();
             services.AddScoped((serviceProvider) => {
@@ -67,7 +54,6 @@ namespace MvcApplication {
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if(env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
