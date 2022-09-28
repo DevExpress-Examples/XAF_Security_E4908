338f2d3dafa37e1ac7e5ee84b2eb945424c8316a
maksim.karpenko <maksim.karpenko@devexpress.com>
2022-09-28 20:34:57 +0300

    Remove business classes unnecessary for security demo

--- EFCore/BusinessObjectsLibrary/BusinessObjects/Employee.cs
+++ EFCore/BusinessObjectsLibrary/BusinessObjects/Employee.cs
@@ -1,9 +1,23 @@
-﻿namespace BusinessObjectsLibrary.BusinessObjects {
-    public class Employee : Person {
+﻿using DevExpress.ExpressApp.DC;
+using System;
+
+namespace BusinessObjectsLibrary.BusinessObjects {
+    public class Employee {
+
+        public virtual int ID { get; set; }
+
+        public virtual String FirstName { get; set; }
+
+        public virtual String LastName { get; set; }
+
+        public virtual DateTime? Birthday { get; set; }
+
+        [FieldSize(255)]
+        public virtual String Email { get; set; }
 
         public virtual Department Department { get; set; }
 
-        public new string FullName {
+        public string FullName {
             get { return $"{FirstName} {LastName}"; }
         }
     }
