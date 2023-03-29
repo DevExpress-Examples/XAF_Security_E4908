ba88ebd017bc9efe5edcecd67c81ac098575b981
apobekiaris <apostolis.bekiaris@gmail.com>
2023-03-29 14:14:32 +0300

    make LoginAsync actual async

--- EFCore/WebApi/API/Security/AuthenticationController.cs
+++ EFCore/WebApi/API/Security/AuthenticationController.cs
@@ -42,19 +42,19 @@ public class AuthenticationController : ControllerBase {
 
     [HttpPost("LoginAsync")]
     [SwaggerOperation("Checks if the user with the specified logon parameters exists in the database. If it does, authenticates this user.", "Refer to the following help topic for more information on authentication methods in the XAF Security System: <a href='https://docs.devexpress.com/eXpressAppFramework/119064/data-security-and-safety/security-system/authentication'>Authentication</a>.")]
-    public Task<ActionResult> LoginAsync([FromBody] [SwaggerRequestBody(@"For example: <br /> { ""userName"": ""Admin"", ""password"": """" }")]
+    public async Task<ActionResult> LoginAsync([FromBody] [SwaggerRequestBody(@"For example: <br /> { ""userName"": ""Admin"", ""password"": """" }")]
         AuthenticationStandardLogonParameters logonParameters) {
         try {
-            var user = _securityAuthenticationService.Authenticate(logonParameters);
-            if (user == null) return Task.FromResult<ActionResult>(Unauthorized("User name or password is incorrect."));
+            var user=await Task.Run(() => _securityAuthenticationService.Authenticate(logonParameters));
+            if (user == null) return Unauthorized("User name or password is incorrect.");
             using var objectSpace = _nonSecuredObjectSpaceFactory.CreateNonSecuredObjectSpace<ApplicationUser>();
             var userOid = new Guid(user.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value);
             var xafUser = objectSpace.FirstOrDefault<ApplicationUser>(u => u.ID == userOid);
             (((ClaimsIdentity)user.Identity)!).AddClaims(new[]{new Claim(nameof(ApplicationUser.ID),xafUser.ID.ToString()),new Claim(nameof(ApplicationUser.IsActive),xafUser.IsActive.ToString()), });
-            return Task.FromResult<ActionResult>(new SignInResult(CookieAuthenticationDefaults.AuthenticationScheme,
-                user, new AuthenticationProperties { AllowRefresh = true, ExpiresUtc = DateTimeOffset.Now.AddDays(1), IsPersistent = true, }));
+            return new SignInResult(CookieAuthenticationDefaults.AuthenticationScheme,
+                user, new AuthenticationProperties { AllowRefresh = true, ExpiresUtc = DateTimeOffset.Now.AddDays(1), IsPersistent = true, });
         } catch(AuthenticationException) {
-            return Task.FromResult<ActionResult>(Unauthorized("User name or password is incorrect."));
+            return Unauthorized("User name or password is incorrect.");
         }
     }
 
