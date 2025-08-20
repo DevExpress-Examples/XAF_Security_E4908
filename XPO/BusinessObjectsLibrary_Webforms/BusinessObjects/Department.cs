71befd0f1ffc14e6bca881a13b5d3ec88190f9e3
Vince Jorolan <vince.jorolan@devsoft.ph>
2025-08-20 16:58:28 +0800

    Rewrite DxDataGrid into DxGrid and update DX versions to 25.1.4

---
+++ XPO/BusinessObjectsLibrary_Webforms/BusinessObjects/Department.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace BusinessObjectsLibrary.BusinessObjects {
+	public class Department : BaseObject {
+		private string title;
+		private string office;
+		public Department(Session session)
+			: base(session) {
+		}
+		public string Title {
+			get {
+				return title;
+			}
+			set {
+				SetPropertyValue(nameof(Title), ref title, value);
+			}
+		}
+		public string Office {
+			get {
+				return office;
+			}
+			set {
+				SetPropertyValue(nameof(Office), ref office, value);
+			}
+		}
+		[Association("Department-Employees")]
+		public XPCollection<Employee> Employees {
+			get {
+				return GetCollection<Employee>(nameof(Employees));
+			}
+		}
+		public override string ToString() {
+			return Title;
+		}
+	}
+}
