6330684ae79b7452dc5431dd5cf340cdd816e630
Vince Jorolan <vince.jorolan@devsoft.ph>
2025-08-26 15:14:38 +0800

    Rewrite everything based on lost changes from previous branch

---
+++ EFCore/ASP.NetCore/Blazor.ServerSide/Models/EditableEmployee.cs
@@ -0,0 +1,9 @@
+﻿using BusinessObjectsLibrary.BusinessObjects;
+
+namespace Blazor.ServerSide.Models;
+public class EditableEmployee {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public Department Department { get; set; }
+}
---
+++ XPO/ASP.NetCore/Blazor.ServerSide/Models/EmployeeExtentions.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObjectsLibrary.BusinessObjects;
+
+namespace Blazor.ServerSide.Models;
+
+public static class EmployeeExtensions {
+    public static EditableEmployee ToModel(this Employee employee) {
+        return new EditableEmployee {
+            FirstName = employee.FirstName,
+            LastName = employee.LastName,
+            Email = employee.Email,
+            Department = employee.Department
+        };
+    }
+    public static void FromModel(this EditableEmployee editableEmployee, Employee employee) {
+        employee.FirstName = editableEmployee.FirstName;
+        employee.LastName = editableEmployee.LastName;
+        employee.Email = editableEmployee.Email;
+        employee.Department = editableEmployee.Department;
+    }
+}
