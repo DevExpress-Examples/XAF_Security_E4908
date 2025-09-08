d11ec07b2417e15dd38662035b68b702ca017c2a
vincejorolan <vince.jorolan@devsoft.ph>
2025-09-08 19:03:18 +0800

    Rewrite DxDataGrid into DxGrid and update DX versions to 25.1.4 (#107)

---
+++ XPO/ASP.NetCore/Blazor.ServerSide/Components/CellEditTemplateBase.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.ExpressApp.Security;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.ServerSide.Components {
+    public class CellEditTemplateBase<T> : ComponentBase {
+        [Parameter] public T CurrentObject { get; set; }
+        [Parameter] public string PropertyName { get; set; }
+        [Inject] private ISecurityProvider SecurityProvider { get; set; }
+
+        private SecurityStrategy Security { get; set; }
+
+        protected string ProtectedContent { get; } = "*******";
+        protected bool CanWrite => CurrentObject is null ? Security.CanWrite(typeof(T), PropertyName) : Security.CanWrite(CurrentObject, PropertyName);
+        protected bool CanRead => CurrentObject is null ? Security.CanRead(typeof(T), PropertyName) : Security.CanRead(CurrentObject, PropertyName);
+
+        protected override void OnInitialized() {
+            Security = (SecurityStrategy)SecurityProvider.GetSecurity();
+            base.OnInitialized();
+        }
+    }
+}
